1eb5dbd62300079cfdc955e456ff5240511a4678
MateusMania <130065876+MateusMania@users.noreply.github.com>
2024-06-28 10:34:38 -0300

    Update and rename Script.js to Mudar Imagens.js

---
+++ Mudar Imagens.js
@@ -0,0 +1,30 @@
+// Cada função abaixo é uma função que muda a imagem do tamagochi para o status atual.
+
+function ImageNormal(filename) {
+  let img = document.querySelector("#tamagochiImage");
+  img.setAttribute("src", filename);
+}
+function ImagemTriste(filename) {
+  let img = document.querySelector("#tamagochiImage");
+  img.setAttribute("src", filename);
+}
+function ImagemFome(filename) {
+  let img = document.querySelector("#tamagochiImage");
+  img.setAttribute("src", filename);
+}
+function ImagemSono(filename) {
+  let img = document.querySelector("#tamagochiImage");
+  img.setAttribute("src", filename);
+}
+function ImageBrincando(filename) {
+  let img = document.querySelector("#tamagochiImage");
+  img.setAttribute("src", filename);
+}
+function ImagemComendo(filename) {
+  let img = document.querySelector("#tamagochiImage");
+  img.setAttribute("src", filename);
+}
+function ImagemDormindo(filename) {
+  let img = document.querySelector("#tamagochiImage");
+  img.setAttribute("src", filename);
+}
