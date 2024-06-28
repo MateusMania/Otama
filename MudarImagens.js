e925784aa15b1004589e606d66e02a5f36749ad2
MateusMania <130065876+MateusMania@users.noreply.github.com>
2024-06-28 10:14:45 -0300

    Create MudarImagens.js

---
+++ MudarImagens.js
@@ -0,0 +1,28 @@
+function ImageNormal(filename) {
+  let img = document.querySelector("#tamagochiImage");
+  img.setAttribute("src", filename);
+}
+function ImagemTriste(filename) {
+  let img = document.querySelector("#tamagochiImage");
+  img.setAttribute("src", filename);
+}
+function ImagemFome(filename) {
+  let img = document.querySelector("#tamagochiImage");
+  img.setAttribute("src", filename);
+}
+function ImagemSono(filename) {
+  let img = document.querySelector("#tamagochiImage");
+  img.setAttribute("src", filename);
+}
+function ImageBrincando(filename) {
+  let img = document.querySelector("#tamagochiImage");
+  img.setAttribute("src", filename);
+}
+function ImagemComendo(filename) {
+  let img = document.querySelector("#tamagochiImage");
+  img.setAttribute("src", filename);
+}
+function ImagemDormindo(filename) {
+  let img = document.querySelector("#tamagochiImage");
+  img.setAttribute("src", filename);
+}
